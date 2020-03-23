b12a34fb366a9f6ade8823128b3b6fa07f4cf4bf
guidoasbun <gdrodrigo8@gmail.com>
2020-03-23 09:50:23 -0700

    postings routes

--- routes/postingRoutes.js
+++ routes/postingRoutes.js
@@ -3,22 +3,30 @@ const { Posting } = require("../models");
 
 //GET all postings
 router.get("/postings", (req, res) => {
-    Posting.find()
+  Posting.find()
+    .then((items) => res.json(items))
+    .catch((e) => console.error(e));
 });
 
 //POST one item
 router.post("/postings", (req, res) => {
-    Posting.create()
+  Posting.create(req.body)
+    .then(() => res.sendStatus(200))
+    .catch((e) => console.error(e));
 });
 
 //PUT one item
 router.put("/postings/:id", (req, res) => {
-    Posting.findByIdAndUpdate()
+  Posting.findByIdAndUpdate(req.params.id, req.body).then(() =>
+    res.sendStatus(200).catch((e) => console.error(e))
+  );
 });
 
 //DELETE one item
 router.delete("/postings/:id", (req, res) => {
-
+  Posting.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch((e) => console.error(e));
 });
 
 module.exports = router;
