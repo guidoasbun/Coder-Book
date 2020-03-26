9afc94ab990a3364ff8f574df6207bff6f375529
cprom <chen.prom@gmail.com>
2020-03-26 16:15:20 -0700

    update ticker

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./components/navbar";
 import Sandbox from "./components/sandbox";
+import Ticker from 'react-ticker'
 
 const App = () => {
+  const [data, setData] = useState([])
   useEffect(() => {
     axios
       //once a user is created in the DB, use a user ID to pull data
@@ -16,19 +18,29 @@ const App = () => {
 
   useEffect(() => {
     axios.get("https://api.chucknorris.io/jokes/random")
-      .then(({ data: facts }) => {
-        console.log(facts.value)
-      })
-      .catch((e) => console.log(e))
-  })
+      .then(result => setData(result.data))
+    console.log(data)
+
+  }, [])
+
 
   return (
     <div>
 
       <Navbar />
-      <Sandbox />
+      < Ticker >
+        {({ index }) => (
+          <>
+            <h1 style={{ whiteSpace: 'nowrap' }}><img src={data.icon_url} alt="" />{data.value}{index}</h1>
+
+          </>
+        )}
+      </Ticker >
+      {/* <Sandbox /> */}
 
       <h1>Hello Project 3!!!</h1>
+
+
     </div>
   );
 };
--- client/src/components/sandbox/sandbox.js
+++ client/src/components/sandbox/sandbox.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Ticker from 'react-ticker'
+import axios from 'axios'
 
 
 
 
 const MoveStuffAround = () => (
 
-  <Ticker>
+  < Ticker >
     {({ index }) => (
       <>
         <h1>This is the Headline of element #{index}!</h1>
         <img src="www.my-image-source.com/" alt="" />
       </>
     )}
-  </Ticker>
+  </Ticker >
 )
 
 export default MoveStuffAround
+
+
+
+
