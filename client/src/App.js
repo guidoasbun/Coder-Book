9d3517dd1f6958ab786b50b68cc2adf97d8cf0bc
guidoasbun <gdrodrigo8@gmail.com>
2020-03-29 22:10:00 -0700

    updating backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import Sandbox from "./components/sandbox";
-import Chuckticker from "./components/chuckticker"
-
+import Chuckticker from "./components/chuckticker";
 
 const App = () => {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    axios
-      //once a user is created in the DB, use a user ID to pull data
-      .get("/api/users/5e7bb027a5c0ba11807b5ae3")
-      .then(({ data: users }) => {
-        console.log(users);
-      })
-      .catch((e) => console.error(e));
-  }, []);
+  // const [data, setData] = useState([]);
 
   return (
     <div>
-
       <Navbar />
-      <Chuckticker />
-      <Sandbox />
-
-
+      {/*<Chuckticker />*/}
+      {/*<Sandbox />*/}
     </div>
   );
 };
--- routes/postingRoutes.js
+++ routes/postingRoutes.js
@@ -2,48 +2,61 @@ const router = require("express").Router();
 const { Posting, User } = require("../models");
 const passport = require("passport");
 
+//Get all postings
+
+// Get all postings from user
+// GET all items from user
+router.get("/postings", (req, res) => {
+    console.log(req)
+  // Posting.find({ owner: req.user._id })
+  //   .then((items) => res.json(items))
+  //   .catch((e) => console.error(e));
+});
+
+/*-------------------------------------------------------------------*/
+
 //Get all postings from user
 // GET all items from user
-router.get("/postings", passport.authenticate("jwt"), (req, res) => {
-  Posting.find({ owner: req.user._id })
-    .then((items) => res.json(items))
-    .catch((e) => console.error(e));
-});
+// router.get("/postings", passport.authenticate("jwt"), (req, res) => {
+//   Posting.find({ owner: req.user._id })
+//     .then((items) => res.json(items))
+//     .catch((e) => console.error(e));
+// });
 
 //POST one item
 // req.user has the user info
-router.post("/postings", passport.authenticate("jwt"), (req, res) => {
-  console.log(req.user);
-  // res.sendStatus(200)
-  Posting.create({
-    entryTitle: req.body.entryTitle,
-    entry: req.body.entry,
-    owner: req.user._id,
-  })
-    .then(({ _id }) => {
-      User.findByIdAndUpdate(req.user._id, {
-        $push: { postings: _id },
-      }).then(() => res.sendStatus(200));
-    })
-    .catch((e) => console.error(e));
-});
+// router.post("/postings", passport.authenticate("jwt"), (req, res) => {
+//   console.log(req.user);
+//   // res.sendStatus(200)
+//   Posting.create({
+//     entryTitle: req.body.entryTitle,
+//     entry: req.body.entry,
+//     owner: req.user._id,
+//   })
+//     .then(({ _id }) => {
+//       User.findByIdAndUpdate(req.user._id, {
+//         $push: { postings: _id },
+//       }).then(() => res.sendStatus(200));
+//     })
+//     .catch((e) => console.error(e));
+// });
 
 //PUT one item
-router.put("/postings/:id", passport.authenticate("jwt"), (req, res) => {
-  Posting.findByIdAndUpdate(req.params.id, req.body).then(({ _id }) => {
-    res.sendStatus(200).catch((e) => console.error(e));
-  });
-});
+// router.put("/postings/:id", passport.authenticate("jwt"), (req, res) => {
+//   Posting.findByIdAndUpdate(req.params.id, req.body).then(({ _id }) => {
+//     res.sendStatus(200).catch((e) => console.error(e));
+//   });
+// });
 
 //DELETE one item
-router.delete("/postings/:id", passport.authenticate("jwt"), (req, res) =>
-  Posting.findByIdAndDelete(req.params.id)
-    .then(({ _id, owner }) => {
-      User.findByIdAndUpdate(owner, { $pull: { postings: _id } }).then(() =>
-        res.sendStatus(200)
-      );
-    })
-    .catch((e) => console.error(e))
-);
+// router.delete("/postings/:id", passport.authenticate("jwt"), (req, res) =>
+//   Posting.findByIdAndDelete(req.params.id)
+//     .then(({ _id, owner }) => {
+//       User.findByIdAndUpdate(owner, { $pull: { postings: _id } }).then(() =>
+//         res.sendStatus(200)
+//       );
+//     })
+//     .catch((e) => console.error(e))
+// );
 
 module.exports = router;
