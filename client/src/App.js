5839bd0a6f4fa2e2164c4709c9d7f37d67cbdbe2
Jean Li <legacy812@gmail.com>
2020-03-30 13:42:52 -0700

    Adding NEWS API

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import Sandbox from "./components/sandbox";
 import Chuckticker from "./components/chuckticker";
+import NewsArea from "./components/NewsArea";
 
 const App = () => {
   // const [data, setData] = useState([]);
@@ -10,6 +11,7 @@ const App = () => {
     <div>
       <Navbar />
       <Chuckticker />
+      <NewsArea />
       {/*<Sandbox />*/}
     </div>
   );
--- client/src/components/NewsArea/NewsArea.js
+++ client/src/components/NewsArea/NewsArea.js
@@ -1,36 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import axios from 'axios'
-import { get } from 'mongoose'
 
 const NewsArea = () => {
-  axios.get("http://newsapi.org/v2/top-headlines?category=technology&sortBy=publishedAt&language=en&apiKey=7561e5f155d14e048633335b62c31dde"
-)
-  .then(r => r.json())
-  .then(({ articles }) => {
-    console.log(articles);
+  const [setData] = useState([]); 
+      axios
+        .get("https://newsapi.org/v2/top-headlines?category=technology&sortBy=publishedAt&language=en&apiKey=7561e5f155d14e048633335b62c31dde")
+        .then((result) => setData(result.data))
+  }, []
 
   return (
-    
-  )
-    document.getElementById("newsTitle0").innerHTML = `
-        <h4>${articles[0].title}</h4>
-      `;
-    document.getElementById("newsBody0").innerHTML = `
-        <p>${articles[0].description}</p>
-      `;
-    document.getElementById("newsTitle1").innerHTML = `
-        <h4>${articles[1].title}</h4>
-      `;
-    document.getElementById("newsBody1").innerHTML = `
-        <p>${articles[1].description}</p>
-      `;
-    document.getElementById("newsTitle2").innerHTML = `
-        <h4>${articles[2].title}</h4>
-      `;
-    document.getElementById("newsBody2").innerHTML = `
-        <p>${articles[2].description}</p>
-      `;
-  })
-  .catch(e => console.error(e));
+      <React.Fragment>
+        <CssBaseline />
+        <Container fixed>
+          <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        </Container>
+      </React.Fragment>
+    );
+  //   <>
+  //   <document.getElementById("newsTitle0").innerHTML = `
+  //       <h4>${articles[0].title}</h4>
+  //     ` />;
+  //   >document.getElementById("newsBody0").innerHTML = `
+  //       <p>${articles[0].description}</p>
+  //     ` />;
+  //   document.getElementById("newsTitle1").innerHTML = `
+  //       <h4>${articles[1].title}</h4>
+  //     ` />;
+  //   document.getElementById("newsBody1").innerHTML = `
+  //       <p>${articles[1].description}</p>
+  //     ` />;
+  //   document.getElementById("newsTitle2").innerHTML = `
+  //       <h4>${articles[2].title}</h4>
+  //     ` />;
+  //   document.getElementById("newsBody2").innerHTML = `
+  //       <p>${articles[2].description}</p>
+  // >)
+  // .catch(e => console.error(e));
+};
 
-  export default NewsArea
+  export default NewsArea;
