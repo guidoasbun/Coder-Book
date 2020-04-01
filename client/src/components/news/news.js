7be929be72fa8a82da2b85b56b531bc3c1e80fe0
Jean Li <legacy812@gmail.com>
2020-04-01 13:17:32 -0700

    News 3.0

--- client/src/components/news/news.js
+++ client/src/components/news/news.js
@@ -4,29 +4,48 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import axios from 'axios'
 
+
+
 const NewsArea = () => {
-  let [data, setData] = useState([]);
+  const [data, setData] = useState([])
+  const [newsPic, setNewsImage] = useState([])
+  const [newsUrl, setNewsUrl] = useState([])
+  const [newsContent, setNewsContent] = useState([])
+
+
+  const fetchData = () => {
+    const newsAPI = 'https://newsapi.org/v2/top-headlines?source=techcrunch&language=en&sortBy=publishedAt&apiKey=7561e5f155d14e048633335b62c31dde'
+
+    const getArticle = axios.get(newsAPI)
+    axios.all([getArticle]).then(
+      axios.spread((...allData) => {
+        const allDataTitle = allData[0].data.articles[1].title
+        const allDataUrl = allData[0].data.articles[1].url
+        const allDataImage = allData[0].data.articles[1].urlToImage
+        const allDataContent = allData[0].data.articles[1].content
+
+        setData(allDataTitle)
+        setNewsUrl(allDataUrl)
+        setNewsImage(allDataImage)
+        setNewsContent(allDataContent)
+      })
+    )
+
+  }
 
   useEffect(() => {
-    axios
-      .get("https://newsapi.org/v2/top-headlines?category=technology&sortBy=publishedAt&language=en&apiKey=7561e5f155d14e048633335b62c31dde")
-      .then(result => {
-        console.log(result.data.articles)
-        setData(result.data.articles)
-      })
 
-
+    fetchData()
   }, [])
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container fixed>
-        {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '10vh' }} /> */}
-        <div>{data.articles}</div>
+    <>
+      <h1>{data}</h1>
+      <img src={newsPic} />
+      <div>{newsUrl}</div>
+      <div>{newsContent}</div>
+    </>
 
-      </Container>
-    </React.Fragment>
   )
 }
 
