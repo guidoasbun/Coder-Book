8335f87c52abb03d6fb62f22f582b234a08a3e28
Jean Li <legacy812@gmail.com>
2020-04-06 13:48:23 -0700

    Menu

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import LoginModal from "../login";
+import TemporaryDrawer from '../drawer/drawer';
 
 
 const useStyles = makeStyles(theme => ({
@@ -77,18 +77,10 @@ export default function SearchAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-
           <Typography className={classes.title} variant="h6" noWrap>
             <img style={{ width: "200px" }} src="/assets/images/coder_book.png" />
           </Typography>
+          <TemporaryDrawer />
           <Button  color="inherit"><LoginModal /></Button>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
